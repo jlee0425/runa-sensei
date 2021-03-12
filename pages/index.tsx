c392b7a99c8b3ef4e9b3abf3ef347012a6897d34
jlee0425 <jake.jklee425@gmail.com>
2021-03-12 16:59:36 +0900

    calendar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { Text } from '@chakra-ui/react';
+import { Box, Center, Grid, GridItem, Text } from '@chakra-ui/react';
 import Calendar from '../components/Calendar';
 
-interface Props {}
-
-const Home = (props: Props) => {
+const Home = () => {
 	return <Calendar />;
 };
 

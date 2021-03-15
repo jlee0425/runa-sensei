ac9441ebaa4738b9fdfacc6732817080393138eb
jlee0425 <jake.jklee425@gmail.com>
2021-03-15 18:57:05 +0900

    refactor

--- components/Calendar/WeeklyCalendar/WeeklyBody.tsx
+++ components/Calendar/WeeklyCalendar/WeeklyBody.tsx
@@ -1,31 +1,18 @@
+import { Grid } from '@chakra-ui/react';
 import React, { useContext } from 'react';
 import { CalendarContext } from '../../../lib/context';
+import { useDaysOfWeek } from '../../../lib/useDaysOfWeek';
 import { CalendarRow } from '../CalendarComponents/CalendarRow';
 import { WeeklyColumn } from './WeeklyColumn';
 
-const getTimeline = (startTime: number, endTime: number) => {
-	const timeline: string[] = [];
-	const [noon, am, pm] = [12, 'AM', 'PM'];
-
-	for (let i = startTime; i < endTime; i++) {
-		if (i < noon) {
-			timeline.push(i + ' ' + am);
-		} else if (i == noon) {
-			timeline.push(i + ' ' + pm);
-		} else {
-			timeline.push(i - noon + ' ' + pm);
-		}
-	}
-	return timeline;
-};
-
 export const WeeklyBody = () => {
-	const [startTime, endTime] = [7, 22];
-	const timeline = getTimeline(startTime, endTime);
+	const days: Date[] = useDaysOfWeek();
 
 	return (
-		<>
-			<WeeklyColumn colNum={1} />
-		</>
+		<Grid templateColumns='repeat(7, 1fr)'>
+			{days.map((day) => (
+				<WeeklyColumn date={day} />
+			))}
+		</Grid>
 	);
 };
